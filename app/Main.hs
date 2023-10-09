b188550da70e9e31635c22f8fde9846d00935de3
Carht <carht@protonmail.com>
2023-10-09 13:05:45 -0400

    #3 Extraer el tipo de archivo usando como entrada la ruta o path.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main (main) where
 
 import Lib
-
+import System.Posix.Files (FileStatus, isDirectory, isRegularFile, isSymbolicLink,
+                          getSymbolicLinkStatus)
+import System.Posix.Types (FileOffset)
 
 data TipoArchivo = Directorio | Archivo | LinkSimbolico | Otro deriving (Show, Eq)
 
@@ -10,6 +12,18 @@ data ArchivoCompleto = Archivos {
   ruta :: FilePath,
   tamano :: FileOffset
   } deriving (Show, Eq)
+
+estatusArchivo :: FileStatus -> TipoArchivo
+estatusArchivo estatus
+  | isDirectory estatus    = Directorio
+  | isSymbolicLink estatus = LinkSimbolico
+  | isRegularFile estatus  = Archivo
+  | otherwise              = Otro
+
+tipoArchivo :: FilePath -> IO TipoArchivo
+tipoArchivo ruta = do
+  estatus <- getSymbolicLinkStatus ruta
+  return $ estatusArchivo estatus
 
 main :: IO ()
 main = someFunc
