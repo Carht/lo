4c19a2ad2847b1e6ad2cb2a6cbd6e26b64eb2368
Carht <carht@protonmail.com>
2023-10-09 12:54:50 -0400

    #2 Estructura de datos para archivo.

--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,11 @@ import Lib
 
 data TipoArchivo = Directorio | Archivo | LinkSimbolico | Otro deriving (Show, Eq)
 
+data ArchivoCompleto = Archivos {
+  tipo :: TipoArchivo,
+  ruta :: FilePath,
+  tamano :: FileOffset
+  } deriving (Show, Eq)
+
 main :: IO ()
 main = someFunc
