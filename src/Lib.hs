fdd4181c4f0fcb714da49a8224c969a197e1cc03
Carht <carht@protonmail.com>
2023-10-16 04:01:08 -0400

    #15 Agregando cálculos para el tamaño de los archivos y con las funciones para salida en formato legible para humanos (kilobytes, megabyte, gigabyte).

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,40 @@
 module Lib
-    ( someFunc
+    ( gigabyteStr,
+      toGigabytes,
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Posix.Types (FileOffset)
+
+kilobyte :: Integral a => a -> a
+kilobyte bytes
+  | bytes <= 1000 = bytes
+  | otherwise = div bytes 1000
+
+kilobyteStr :: (Show a, Integral a) => a -> String
+kilobyteStr bytes
+  | bytes <= 1000 = show bytes
+  | otherwise = show (kilobyte bytes) <> "K"
+
+megabyte :: Integral a => a -> a
+megabyte bytes
+  | bytes <= 1000000 = kilobyte bytes
+  | otherwise = div (kilobyte bytes) 1000
+
+megabyteStr :: (Show a, Integral a) => a -> String
+megabyteStr bytes
+  | bytes <= 1000000 = kilobyteStr bytes
+  | otherwise = show (megabyte bytes) <> "M"
+
+gigabyte :: Integral a => a -> a
+gigabyte bytes
+  | bytes <= 1000000000 = megabyte bytes
+  | otherwise = div (megabyte bytes) 1000
+
+gigabyteStr :: (Show a, Integral a) => a -> String
+gigabyteStr bytes
+  | bytes <= 1000000000 = megabyteStr bytes
+  | otherwise = show (gigabyte bytes) <> "G"
+
+toGigabytes :: (Show b, Integral b) => [(String, b)] -> [[String]]
+toGigabytes [] = []
+toGigabytes (x:xs) = [fst x, gigabyteStr . snd $ x] :  toGigabytes xs
